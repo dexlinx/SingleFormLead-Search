34fef2d0c8a98e0cb14daf1fedf7ab2b349c75d3
Tony Rappa <dexlinx@gmail.com>
2017-11-13 17:47:58 -0800

    Update singleformresponse.php

--- singleformresponse.php
+++ singleformresponse.php
@@ -9,9 +9,9 @@
 
 //Variables
 //-------------------------------------------------------------
-$apiKey = ""; //Client API Key
+$apiKey = "2wEjn7BA2xFtVeR_VGINZp"; //Client API Key
 $idxID = "b092"; //MLS ID
-$idxUrl = ""; //IDX Url
+$idxUrl = "http://www.naparealestate.idxbroker.com";
 
 
 //Sanitize the POST variable
@@ -48,6 +48,122 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 60,
   CURLOPT_POSTFIELDS => $data,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => $callType,
+  CURLOPT_HTTPHEADER => array(
+    "accesskey: ".$apiKey,
+    "apiversion: null",
+    "cache-control: no-cache",
+    "content-type: application/x-www-form-urlencoded",
+    "outputtype: json"
+    ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+$decode = json_decode($response,true);
+return $decode;
+}
+}
+
+//Lead Creation
+//-------------------------------------------------------------
+
+    $data = array(
+      'firstName' => $sanPost["firstName"],
+      'lastName' => $sanPost["lastName"],
+      'email' => $sanPost["email"],
+	  'phone' => $sanPost["phone"],
+	  'password' => $sanPost["password"]
+    );	
+	
+    $data = http_build_query($data); // encode and & delineate
+
+
+$createLead = apiCall($apiKey,'PUT','leads/lead',$data);
+
+if ($createLead != 'Lead already exists.'){ //Lead Doesn't Exist - We create it
+
+	$leadId = $createLead[newID]; //Save the Returned Lead ID
+
+}else{ //Lead Exists, Now we have to find its ID
+	
+	
+//Get chunks of X Number of leads and Locate Lead ID
+//-------------------------------------------------------------
+$offset = 0;
+
+do{
+	
+	$currentLeads = apiCall($apiKey,'GET','leads/lead?rf[]=email&rf[]=id&offset='.$offset.'&limit=350',$data);
+	$offset+=350; //<- Limit and Offset Determine Number of leads per loop
+
+if($currentLeads != NULL){
+	foreach($currentLeads as $key => $value){ //Loop Through This Set of Leads
+		if ($sanPost[email] == $value["email"]){
+		$match = 'yes';
+		$leadId = $value["id"]; //Keep the Existing Lead ID for later
+		echo $leadId;
+		echo $match;
+		}
+	}
+}
+
+}while($match != 'yes' && $currentLeads != NULL);
+}
+
+
+//Getting the List of Cities
+//-----------------------------------------------------------------------------------
+$cities = $sanPost['cities'];
+
+//Add the Saved Search
+$stripLp = str_replace(",", "",$sanPost['lp']);
+$stripHp = str_replace(",", "",$sanPost['hp']);
+
+//-----------------------------------------------------------------------------------
+$searchArray = array('idxID' => $idxID,'lp' => $stripLp, 'hp' => $stripHp,'bd' => $sanPost['bd'],'tb' => $sanPost['ba'],'sqft' => $sanPost['sqFt'],'acres' => $sanPost['acres'],'city' => $cities);
+
+$data = array(
+ 'searchName' => 'My Saved Search',
+ 'search' => $searchArray
+);
+	
+    $data = http_build_query($data); // encode and & delineate
+
+	
+$endPoint = 'leads/search/'.$leadId;
+apiCall($apiKey,'PUT',$endPoint,$data);
+
+
+
+//Create City list for Redirect URL
+//-----------------------------------------------------------------------------------
+if (isset($sanPost['cities'])){
+$myCities = implode("&city[]=",$cities);
+$searchCities = "&city[]=".$myCities;
+}
+
+//Redirect to IDX Search
+//-----------------------------------------------------------------------------------
+$redirectUrl = $idxUrl."/idx/results/listings?idxID=".$idxID."&lp=".$sanPost['lp']."&hp=".$sanPost['hp']."&bd=".$sanPost['bd']."&tb=".$sanPost['ba']."&sqft=".$sanPost['sqFt']."&acres=".$sanPost['acres'].$searchCities;
+
+echo "<center>Performing Search . . .";
+
+echo "<script>";
+echo "window.location.href= '".$redirectUrl."';";
+echo "</script>";
+
+?>
+
+</body>
+</html>
+
   CURLOPT_CUSTOMREQUEST => $callType,
   CURLOPT_HTTPHEADER => array(
     "accesskey: ".$apiKey,
